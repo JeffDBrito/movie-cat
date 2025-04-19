a8f16430140468eb847bf9b26e20628df5c04d90
Jefferson Brito <jeffersondanilo2517@gmail.com>
2025-04-19 00:25:56 -0300

    Criando modelo e migration do Filme

---
+++ backend/database/migrations/2025_04_19_024636_create_filmes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('filmes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('tmdb_id')->unique();
+            $table->string('title');
+            $table->string('poster_path')->nullable();
+            $table->json('tmdb_details');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('filmes');
+    }
+};
