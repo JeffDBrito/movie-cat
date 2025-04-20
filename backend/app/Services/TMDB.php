d9839aa8fa064908b0d661dc4ea818be5a1690ea
Jefferson Brito <jeffersondanilo2517@gmail.com>
2025-04-20 19:09:08 -0300

    Adicionando novos métodos de busca no serviço do TMDB

--- backend/app/Services/TMDB.php
+++ backend/app/Services/TMDB.php
@@ -17,6 +17,10 @@ class TMDB {
         $this->imageUrl = config('tmdb.image_url');
     }
 
+    /**
+     * Busca detalhes de um filme pelo ID do TMDB.
+     * @param mixed $tmdbId
+     */
     public function getFilme($tmdbId)
     {
 
@@ -31,16 +35,59 @@ class TMDB {
 
 
                 $data = json_decode($response->getBody(), true);
-                return [
-                    'tmdb_id' => $data['id'],
-                    'title' => $data['title'],
-                    'poster_path' => $data['poster_path'],
-                    'tmdb_details' => $data,
-                ];
+                return $data;
             }
         }catch (\Exception $e) {
             Log::error("Erro ao buscar filme no TMDB: " . $e->getMessage());
             return null;
         }
     }
+
+    /**
+     * Busca filmes por categoria.
+     * @param mixed $categoria
+     * @param mixed $page
+     */
+    public function getFilmesPorCategoria($categoria, $page = 1)
+    {
+        $url = "{$this->baseUrl}/movie/{$categoria}?api_key={$this->apiKey}&language=pt-BR&page={$page}";
+
+        try{
+            $client = new Client();
+
+            $response = $client->get($url);
+
+            if ($response->getStatusCode() === 200) {
+                $data = json_decode($response->getBody(), true);
+                return $data;
+            }
+        }catch (\Exception $e) {
+            Log::error("Erro ao buscar filmes por categoria no TMDB: " . $e->getMessage());
+            return null;
+        }
+    }
+
+    /**
+     * Busca filmes por título.
+     * @param mixed $titulo
+     */
+    public function buscarPorTitulo($titulo)
+    {
+        $url = "{$this->baseUrl}/search/movie?api_key={$this->apiKey}&query={$titulo}&language=pt-BR";
+
+        try{
+            $client = new Client();
+
+            $response = $client->get($url);
+
+            if ($response->getStatusCode() === 200) {
+                $data = json_decode($response->getBody(), true);
+                return $data;
+            }
+        }catch (\Exception $e) {
+            Log::error("Erro ao buscar filme por título no TMDB: " . $e->getMessage());
+            return null;
+        }
+    }
+
 }
