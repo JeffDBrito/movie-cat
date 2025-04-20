bee953b4c5d24d0c39e8880baef77f3273959768
Jefferson Brito <jeffersondanilo2517@gmail.com>
2025-04-20 20:00:26 -0300

    Adicionando métodos de favoritos no controlador de filmes

--- backend/app/Http/Controllers/Filme/FilmeController.php
+++ backend/app/Http/Controllers/Filme/FilmeController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Filme;
 use App\Models\User;
 use App\Services\TMDB;
+use Auth;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
@@ -54,14 +55,49 @@ class FilmeController extends Controller
      */
     public function filmesPorCategoria(Request $request, string $categoria): JsonResponse
     {
+
+        $user = Auth::user();
+        $user_filmes = [];
+        if ($user) {
+            $user_filmes = $user->favoritos()->pluck('tmdb_id')->toArray();
+        }
+
         $page = $request->input('page', 1);
         $filmes = $this->tmdb->getFilmesPorCategoria($categoria, $page);
 
-        foreach ($filmes['results'] as $filme) {
-            $filme['title'] = json_encode($filme['title'], JSON_UNESCAPED_UNICODE);
+        $response = [];
+
+        if (isset($filmes['results'])) {
+            $response['results'] = [];
+        } else {
+            return response()->json(['error' => 'Nenhum filme encontrado.'], 404);
         }
 
-        return response()->json($filmes, 200);
+        // Formata a resposta e adiciona o campo is_favorito
+        foreach ($filmes['results'] as $filme) {
+            $response['page'] = $filmes['page'];
+            $response['total_pages'] = $filmes['total_pages'];
+            $response['total_results'] = $filmes['total_results'];
+            $response['results'][] = [
+                'id' => $filme['id'],
+                'title' => json_encode($filme['title'], JSON_UNESCAPED_UNICODE),
+                'poster_path' => $filme['poster_path'],
+                'is_favorito' => in_array($filme['id'], $user_filmes),
+                'release_date' => $filme['release_date'],
+                'overview' => $filme['overview'],
+                'vote_average' => $filme['vote_average'],
+                'vote_count' => $filme['vote_count'],
+                'backdrop_path' => $filme['backdrop_path'],
+                'original_language' => $filme['original_language'],
+                'original_title' => $filme['original_title'],
+                'genre_ids' => $filme['genre_ids'],
+                'adult' => $filme['adult'],
+                'video' => $filme['video'],
+                'popularity' => $filme['popularity'],
+            ];
+        }
+
+        return response()->json($response, 200);
     }
 
     /**
@@ -80,6 +116,83 @@ class FilmeController extends Controller
         }
 
         return response()->json($filmes, 200);
+    }
+
+    /**
+     * Adiciona um filme aos favoritos do usuário.
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function favoritar(Request $request): JsonResponse
+    {
+
+        $user = Auth::user();
+
+        if (!$user) {
+            return response()->json(['error' => 'Usuário não autenticado.'], 401);
+        }
+
+        $request->validate([
+            'tmdb_id' => 'required|integer',
+        ]);
+
+        $tmdb_id = $request->input('tmdb_id');
+
+        // Verifica se o filme já está registrado no banco de dados
+        $filme = Filme::where('tmdb_id', $tmdb_id)->first();
+
+        // Se o filme não estiver registrado, busca os detalhes no TMDB e registra
+        if(!$filme){
+
+            $filme = $this->tmdb->getFilme($tmdb_id);
+            $filme_json = $this->store($filme);
+
+            if ($filme_json->getStatusCode() !== 201) {
+                return response()->json(['error' => 'Falha ao registrar filme.'], 500);
+            }
+
+            $filme = Filme::where('tmdb_id', $tmdb_id)->first();
+            if (!$filme) {
+                return response()->json(['error' => 'Falha ao registrar filme.'], 500);
+            }
+
+        }
+
+        $user->favoritos()->attach($filme->id);
+
+        return response()->json(['message' => 'Filme adicionado aos favoritos.'], 200);
+    }
+
+    /**
+     * Remove um filme dos favoritos do usuário.
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function desfavoritar(Request $request): JsonResponse
+    {
+
+        $user = Auth::user();
+
+        if (!$user) {
+            return response()->json(['error' => 'Usuário não autenticado.'], 401);
+        }
+
+        $request->validate([
+            'tmdb_id' => 'required|integer',
+        ]);
+
+        $tmdb_id = $request->input('tmdb_id');
+
+        // Verifica se o filme está
+        $filme = Filme::where('tmdb_id', $tmdb_id)->first();
+
+        if (!$filme) {
+            return response()->json(['error' => 'Filme não encontrado.'], 404);
+        }
+
+        $user->favoritos()->detach($filme->id);
+
+        return response()->json(['message' => 'Filme removido dos favoritos.'], 200);
     }
 
 }
