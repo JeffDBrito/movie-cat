1d1f6282f9fbf10e7da76769b297b9d3d19e9712
Jefferson Brito <jeffersondanilo2517@gmail.com>
2025-04-19 21:08:50 -0300

    Adicionando valores padrões para variáveis de ambiente do tmdb

--- backend/config/tmdb.php
+++ backend/config/tmdb.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'api_key' => env('TMDB_API_KEY'),
-    'api_url' => env('TMDB_API_URL'),
-    'image_url' => env('TMDB_IMAGE_URL'),
+    'api_key' => env('TMDB_API_KEY', ''),
+    'api_url' => env('TMDB_API_URL', 'https://api.themoviedb.org/3'),
+    'image_url' => env('TMDB_IMAGE_URL', 'https://image.tmdb.org/t/p/w500'),
 ];
