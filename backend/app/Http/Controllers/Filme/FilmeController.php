a69b2a49ab150f295771f116dd983b2d9d80252b
Jefferson Brito <jeffersondanilo2517@gmail.com>
2025-04-20 19:09:35 -0300

    Adicionando novos métodos de busca no controlador de filmes

--- backend/app/Http/Controllers/Filme/FilmeController.php
+++ backend/app/Http/Controllers/Filme/FilmeController.php
@@ -8,9 +8,17 @@ use App\Models\User;
 use App\Services\TMDB;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class FilmeController extends Controller
 {
+
+    private $tmdb;
+
+    public function __construct()
+    {
+        $this->tmdb = new TMDB();
+    }
 
     /**
      * Registra um filme no banco de dados.
@@ -36,6 +44,42 @@ class FilmeController extends Controller
             return response()->json(['error' => 'Falha ao registrar filme.'], 500);
         }
 
+    }
+
+    /**
+     * Retorna os filmes de uma categoria.
+     * @param Request $request
+     * @param string $categoria
+     * @return JsonResponse
+     */
+    public function filmesPorCategoria(Request $request, string $categoria): JsonResponse
+    {
+        $page = $request->input('page', 1);
+        $filmes = $this->tmdb->getFilmesPorCategoria($categoria, $page);
+
+        foreach ($filmes['results'] as $filme) {
+            $filme['title'] = json_encode($filme['title'], JSON_UNESCAPED_UNICODE);
+        }
+
+        return response()->json($filmes, 200);
+    }
+
+    /**
+     * Busca filmes por título.
+     * @param Request $request
+     * @param string $title
+     * @return JsonResponse
+     */
+    public function buscarPorTitulo(string $title): JsonResponse
+    {
+
+        $filmes = $this->tmdb->buscarPorTitulo($title);
+
+        foreach ($filmes['results'] as $filme) {
+            $filme['title'] = json_encode($filme['title'], JSON_UNESCAPED_UNICODE);
+        }
+
+        return response()->json($filmes, 200);
     }
 
 }
