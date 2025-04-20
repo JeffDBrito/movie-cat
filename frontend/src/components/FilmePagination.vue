3fc7c3ec6773404d4912eae92cc9877ce89cb9a3
Jefferson Brito <jeffersondanilo2517@gmail.com>
2025-04-20 19:03:00 -0300

    Criando component Filme Pagination

---
+++ frontend/src/components/FilmePagination.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+import { defineProps } from 'vue'
+
+const props = defineProps({
+    pagination: {
+        type: Object,
+        required: true,
+        default:({
+            currentPage: 1,
+            totalPages: 1,
+            totalItems: 1,
+            itemsPerPage: 20,
+        })
+    },
+})
+
+</script>
+
+<template>
+
+    <div class="grid grid-cols-6 gap-4">
+        <div class="col-span-2">
+            <button v-if="pagination.currentPage > 1" @click="$emit('paginate-prev')" :disabled="pagination.currentPage === 1">
+                <UIcon name="ooui:arrow-next-rtl"/>
+            </button>
+        </div>
+        <div class="col-span-2">
+            <span>Page {{ pagination.currentPage }} of {{ pagination.totalPages }}</span>
+        </div>
+        <div class="col-span-2">
+            <button v-if="pagination.currentPage < pagination.totalPages" @click="$emit('paginate-next')" :disabled="pagination.currentPage === pagination.totalPages">
+                <UIcon name="ooui:arrow-next-ltr"/>
+            </button>
+        </div>
+    </div>
+
+</template>
