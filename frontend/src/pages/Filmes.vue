b198085af5636ea8af89611bc203f59421a10f7d
Jefferson Brito <jeffersondanilo2517@gmail.com>
2025-04-20 19:04:03 -0300

    Criando página Filmes

---
+++ frontend/src/pages/Filmes.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+import { auth } from '../stores/auth';
+
+import FilmeGallery from '../components/FilmeGallery.vue';
+
+
+</script>
+<template>
+
+    <div class="filmes-page">
+
+        <div>
+            <div class="mb-10">
+                <h1>Filmes</h1>
+            </div>
+            
+            <div class=" items-center py-5 px-10 rounded-lg">
+                
+                <FilmeGallery tipo="grid" titulo="Populares" api_url="api/filmes/categoria/popular" :enablePagination="true"></FilmeGallery>
+                
+            </div>
+        </div>
+        
+    </div>
+
+</template>
