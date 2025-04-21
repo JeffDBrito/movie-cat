2e14a8da0655e6aefefca954b2ec67c28abd1300
Jefferson Brito <jeffersondanilo2517@gmail.com>
2025-04-21 18:22:48 -0300

    Adiciona página MeusFilmes

---
+++ frontend/src/pages/MeusFilmes.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+import FilmeGallery from '../components/FilmeGallery.vue';
+
+</script>
+<template>
+
+    <div class="w-full">
+        <div>
+            <div class="mb-10">
+                <h1>Favoritos</h1>
+            </div>
+
+            <div class=" items-center py-5 px-10 rounded-lg">
+                <FilmeGallery tipo="grid" titulo="Resultados" api_url="api/filmes/meus-filmes" source="database" :enableFilters="true" :enablePagination="true" :enableOrdem="false" :enableAno="false"></FilmeGallery>
+            </div>            
+        </div>        
+    </div>
+
+</template>
