e71118351963e40195cf75401e6f77d31fd625b4
Jefferson Brito <jeffersondanilo2517@gmail.com>
2025-04-21 10:59:56 -0300

    Adicionando métodos de busca no TMDB Service

--- backend/app/Services/TMDB.php
+++ backend/app/Services/TMDB.php
@@ -28,12 +28,8 @@ class TMDB {
 
         try{
             $client = new Client();
-
             $response = $client->get($url);
-
             if ($response->getStatusCode() === 200) {
-
-
                 $data = json_decode($response->getBody(), true);
                 return $data;
             }
@@ -71,9 +67,9 @@ class TMDB {
      * Busca filmes por título.
      * @param mixed $titulo
      */
-    public function buscarPorTitulo($titulo)
+    public function buscarPorTitulo($titulo, $page = 1)
     {
-        $url = "{$this->baseUrl}/search/movie?api_key={$this->apiKey}&query={$titulo}&language=pt-BR";
+        $url = "{$this->baseUrl}/search/movie?api_key={$this->apiKey}&query={$titulo}&language=pt-BR&page={$page}";
 
         try{
             $client = new Client();
@@ -89,5 +85,53 @@ class TMDB {
             return null;
         }
     }
+
+    /**
+     * Busca generos disponíveis.
+     * @return array|null
+     */
+    public function getGeneros()
+    {
+        $url = "{$this->baseUrl}/genre/movie/list?api_key={$this->apiKey}&language=pt-BR";
+        try{
+            $client = new Client();
+            $response = $client->get($url);
+            if ($response->getStatusCode() === 200) {
+                $data = json_decode($response->getBody(), true);
+                return $data;
+            }
+        }catch (\Exception $e) {
+            Log::error("Erro ao buscar gêneros no TMDB: " . $e->getMessage());
+            return null;
+        }
+    }
+
+
+    /**
+     * Busca filmes por gênero.
+     * @param array $generos
+     * @param bool $adults
+     * @param int $page
+     * @param string $sort
+     * @param string $order
+     */
+    public function buscarPorGenero($generos, $page = 1, $adult = true, $ano = null, $ordem = 'popularity.desc')
+    {
+        $generos = is_array($generos) ? implode(',', $generos) : $generos;
+        $url = "{$this->baseUrl}/discover/movie?api_key={$this->apiKey}&include_adult={$adult}&language=pt-BR&page={$page}&sort_by={$ordem}&with_genres={$generos}&primary_release_year={$ano}";
+        try{
+            $client = new Client();
+            $response = $client->get($url);
+            if ($response->getStatusCode() === 200) {
+                $data = json_decode($response->getBody(), true);
+                return $data;
+            }
+        }catch (\Exception $e) {
+            Log::error("Erro ao buscar filmes por gênero no TMDB: " . $e->getMessage());
+            return null;
+        }
+    }
+
+
 
 }
