f621969178177cb633f682b5d4584684ebb91197
Jefferson Brito <jeffersondanilo2517@gmail.com>
2025-04-21 10:41:11 -0300

    Altera a galeria de filmes

--- frontend/src/pages/Filmes.vue
+++ frontend/src/pages/Filmes.vue
@@ -1,29 +1,20 @@
 <script setup>
 
-import { ref, onMounted } from 'vue';
-import { useRoute } from 'vue-router';
-import { auth } from '../stores/auth';
-
 import FilmeGallery from '../components/FilmeGallery.vue';
-
 
 </script>
 <template>
 
-    <div class="filmes-page">
-
+    <div class="w-full">
         <div>
             <div class="mb-10">
                 <h1>Filmes</h1>
             </div>
-            
-            <div class=" items-center py-5 px-10 rounded-lg">
-                
-                <FilmeGallery tipo="grid" titulo="Populares" api_url="api/filmes/categoria/popular" :enablePagination="true"></FilmeGallery>
-                
-            </div>
-        </div>
-        
+
+            <div class=" items-center py-5 px-10 rounded-lg">                
+                <FilmeGallery tipo="grid" titulo="Resultados" api_url="api/filmes/categoria/popular" :enableFilters="true" :enablePagination="true"></FilmeGallery>
+            </div>            
+        </div>        
     </div>
 
 </template>
